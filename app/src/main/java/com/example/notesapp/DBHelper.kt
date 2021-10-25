0d1ed9f4fde62c484d8bab17987717397e0a7785
RAM111111 <r.r.99@windowslive.com>
2021-10-25 12:29:23 +0300

    Retrieve Note

--- app/src/main/java/com/example/notesapp/DBHelper.kt
+++ app/src/main/java/com/example/notesapp/DBHelper.kt
@@ -1,16 +1,21 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context):SQLiteOpenHelper(context,"Note.db",null,1) {
-   var sdb:SQLiteDatabase = writableDatabase
+   private var sdb:SQLiteDatabase = writableDatabase
     override fun onCreate(p0: SQLiteDatabase?) {
         if (p0 != null) {
             p0.execSQL("create table student (note text)")
+
+
         }
+
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
@@ -23,4 +28,16 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,"Note.db",null,1) {
         return status
 
     }
+    @SuppressLint("Range")
+    fun retriveData():ArrayList<String>{
+        var al = arrayListOf<String>()
+        var c : Cursor = sdb.query("student",null,null, null,null,null,null)
+        if (c.moveToFirst()) {
+            do {
+                al.add(c.getString(c.getColumnIndex("note")));
+            } while (c.moveToNext());
+        }
+        return al
+    }
+
 }
--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -3,32 +3,58 @@ package com.example.notesapp
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn:Button
     lateinit var et1:EditText
     var note = ""
+    var nn = ""
+    lateinit var rv:RecyclerView
+    lateinit var all:ArrayList<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn = findViewById(R.id.button)
         et1 = findViewById(R.id.editTextTextPersonName)
+        all = arrayListOf()
+        rv = findViewById(R.id.rv)
+        var dbhlpr = DBHelper(this)
+        all = dbhlpr.retriveData()
+        rv.adapter?.notifyDataSetChanged()
+
+
+
+        Log.d("uuu","${dbhlpr.retriveData()}")
+
 
         btn.setOnClickListener {
 
             note = et1.text.toString()
+            if(note.isNotEmpty()) {
+                var status = dbhlpr.saveddata(note)
+                it.hideKeyboard()
+                Toast.makeText(this, "data Saved successfully +$status", Toast.LENGTH_SHORT).show()
+                all.add(note)
+                rv.adapter?.notifyDataSetChanged()
+                et1.text.clear()
+            }else{
+                Toast.makeText(this, "Type a note", Toast.LENGTH_SHORT).show()
 
-            var dbhlpr = DBHelper(this)
-            var status =  dbhlpr.saveddata(note)
-            it.hideKeyboard()
-            Toast.makeText(this,"data Saved successfully +$status", Toast.LENGTH_SHORT).show()
+            }
 
         }
+        rv.adapter = MyAdapter(all)
+        rv.layoutManager = LinearLayoutManager(this)
     }
     fun View.hideKeyboard() {
         val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
